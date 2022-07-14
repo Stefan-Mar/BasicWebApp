984c7ef545747df4b817b0526219475c0e538f05
Stefan Marzinski <stefanmar.sm@gmail.com>
2022-07-14 13:19:35 +0200

    fixed add

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -16,6 +16,16 @@ public class QueryProcessor {
         } else if (query.contains("what is")) {
             query = query.substring(8);
             int n = query.indexOf('+');
+            if (n == -1) return "";
+            String a = query.substring(0, n);
+            String b = query.substring(n+1);
+            int x = Integer.parseInt(a);
+            int y = Integer.parseInt(b);
+            return "" + (x + y);
+        } else if (query.contains("which")) {
+            query = query.substring(8);
+            int n = query.indexOf('+');
+            if (n == -1) return "";
             String a = query.substring(0, n);
             String b = query.substring(n+1);
             int x = Integer.parseInt(a);
